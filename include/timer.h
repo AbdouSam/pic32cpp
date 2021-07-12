b05464bed05ec049fbc18eb3b1c5acbc35a87ab8
abdellah alaouchiche <abdou.sam7@gmail.com>
2021-07-12 13:19:18 +0100

    timer.* Add singleshot timer, separate set period and start into 2 functions

--- include/timer.h
+++ include/timer.h
@@ -1,56 +1,21 @@
 #ifndef INCLUDED_PIC32_TIMER_H
 #define INCLUDED_PIC32_TIMER_H
 
-typedef enum
-{
-  PIC32_TIMER_1,
-  PIC32_TIMER_2,
-  PIC32_TIMER_3,
-  PIC32_TIMER_4,
-  PIC32_TIMER_5,
-  PIC32_TIMER_6,
-  PIC32_TIMER_7,
-  PIC32_TIMER_8,
-  PIC32_TIMER_9,
-  PIC32_TIMER_MAX,
-}pic32_timer_t;
-
-typedef enum
-{
-  TMR1_PRESCALE_1   = 0b00,
-  TMR1_PRESCALE_8   = 0b01,
-  TMR1_PRESCALE_64  = 0b10,
-  TMR1_PRESCALE_256 = 0b11,
-}timer1_prescaler_t;
-
-/* timers from 2 to 9. */
-
-typedef enum
-{
-  TMR_PRESCALE_1    = 0b000,
-  TMR_PRESCALE_2    = 0b001,
-  TMR_PRESCALE_4    = 0b010,
-  TMR_PRESCALE_8    = 0b011,
-  TMR_PRESCALE_16   = 0b100,
-  TMR_PRESCALE_32   = 0b101,
-  TMR_PRESCALE_64   = 0b110,
-  TMR_PRESCALE_256  = 0b111,
-  TMR_PRESCALE_MAX_INDX  = 8,
-}timers_prescaler_t;
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include "interrupt.h"
+
 void timer_1_init(void);
 
-int timer_init(pic32_timer_t timer_id,
-               uint32_t freq,
-               uint8_t subp,
+int timer_init(int timer_id,
                int_callback_ft intcb);
 
-int timer_stop(pic32_timer_t timer_id);
+int timer_start(int timer_id);
+int timer_start_oneshot(int timer_id);
+int timer_stop(int timer_id);
+int timer_setperiod_ms(int timer_id, uint32_t prd_ms);
 
 
 #ifdef __cplusplus
--- src/timer.c
+++ src/timer.c
@@ -5,6 +5,8 @@
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
+
 #include "pic32_config.h"
 #include "sysclk.h"
 #include "interrupt.h"
@@ -32,7 +34,47 @@
 #define PR(x)               PR ## x
 #define TMR(x)              TMR ## x
 
+#define is_idvalid(x) if ((x) < PIC32_TIMER_2 || (x) > PIC32_TIMER_9) return (-1);
+
 #define MAX_TMR_PR_VALUE (65535U)
+#define DEFAULT_SUB_PRIO 1
+
+enum
+{
+  PIC32_TIMER_1 = 0,
+  PIC32_TIMER_2,
+  PIC32_TIMER_3,
+  PIC32_TIMER_4,
+  PIC32_TIMER_5,
+  PIC32_TIMER_6,
+  PIC32_TIMER_7,
+  PIC32_TIMER_8,
+  PIC32_TIMER_9,
+  PIC32_TIMER_MAX,
+};
+
+enum
+{
+  TMR1_PRESCALE_1   = 0b00,
+  TMR1_PRESCALE_8   = 0b01,
+  TMR1_PRESCALE_64  = 0b10,
+  TMR1_PRESCALE_256 = 0b11,
+};
+
+/* timers from 2 to 9. */
+
+enum
+{
+  TMR_PRESCALE_1    = 0b000,
+  TMR_PRESCALE_2    = 0b001,
+  TMR_PRESCALE_4    = 0b010,
+  TMR_PRESCALE_8    = 0b011,
+  TMR_PRESCALE_16   = 0b100,
+  TMR_PRESCALE_32   = 0b101,
+  TMR_PRESCALE_64   = 0b110,
+  TMR_PRESCALE_256  = 0b111,
+  TMR_PRESCALE_MAX_INDX  = 8,
+};
 
 static uint16_t tmr1_prescale_value[] = {
   1, 8, 64, 256
@@ -79,6 +121,8 @@ static uint32_t volatile * const tmr_tmr[PIC32_TIMER_MAX] =
   &TMR8,
   &TMR9,
 };
+
+static bool timer_int_oneshot[PIC32_TIMER_MAX];
 
 static intr_regs_t const timer_intreg[PIC32_TIMER_MAX] =
 {
@@ -195,29 +239,26 @@ void timer_1_init(void)
   BIT_SET(tmr_con[PIC32_TIMER_1], TMR_ON_BIT);
 }
 
-int timer_init(pic32_timer_t timer_id,
-               uint32_t freq,
-               uint8_t subp,
-               int_callback_ft intcb)
+int timer_setperiod_ms(int timer_id, uint32_t prd_ms)
 {
+  int prs_idx;
   uint32_t tmr_clk;
   uint32_t tmr_preload;
-  int prs_idx;
+  double freq;
 
-  if (timer_id < PIC32_TIMER_2 || timer_id > PIC32_TIMER_9)
-  {
-    return -1;
-  }
+  is_idvalid(timer_id);
 
-  *tmr_con[timer_id] = 0x0;
-  *tmr_tmr[timer_id] = 0x0;
+  if (prd_ms < 1)
+    return -1; /* minimum of period 1 ms */
 
+  freq = (1.0 / (double)prd_ms) * 1000.0;
+  
   tmr_clk = sysclk_timerfreq_get();
 
   /* check if a prescaler is required */
   for (prs_idx = 0; prs_idx < TMR_PRESCALE_MAX_INDX; prs_idx++)
   {
-    tmr_preload = tmr_clk / freq / tmrs_prescale_value[prs_idx];
+    tmr_preload = tmr_clk / (uint32_t)freq / tmrs_prescale_value[prs_idx];
 
     if (tmr_preload <= MAX_TMR_PR_VALUE && tmr_preload > 0)
       break;
@@ -230,8 +271,40 @@ int timer_init(pic32_timer_t timer_id,
   }
 
   *tmr_loadval[timer_id] = tmr_clk / freq / tmrs_prescale_value[prs_idx];
-
   BIT_MASK(tmr_con[timer_id], TMR_TCKPS_MASK, (prs_idx << TMR_TCKPS_BIT_1));
+
+  return 0;
+}
+
+int timer_start(int timer_id)
+{
+  is_idvalid(timer_id);
+
+  BIT_SET(tmr_con[timer_id], TMR_ON_BIT);
+
+  timer_int_oneshot[timer_id] = false;
+
+  return 0;
+}
+
+int timer_start_oneshot(int timer_id)
+{
+  is_idvalid(timer_id);
+
+  BIT_SET(tmr_con[timer_id], TMR_ON_BIT);
+
+  timer_int_oneshot[timer_id] = true;
+  
+  return 0;
+}
+
+int timer_init(int timer_id,
+               int_callback_ft intcb)
+{
+  is_idvalid(timer_id);
+
+  *tmr_con[timer_id] = 0x0;
+  *tmr_tmr[timer_id] = 0x0;
 
 
   BIT_CLR(timer_intreg[timer_id].enable, timer_intreg[timer_id].ibit);
@@ -244,24 +317,20 @@ int timer_init(pic32_timer_t timer_id,
 
   BIT_WRITE(timer_intreg[timer_id].prio, INT_SUBP_BITLEN,
             timer_intreg[timer_id].subpshift,
-            subp);
-
-  BIT_SET(timer_intreg[timer_id].enable, timer_intreg[timer_id].ibit);
+            DEFAULT_SUB_PRIO);
 
   if (intcb != NULL)
+  {
     timer_int_cb[timer_id] = intcb;
-
-  BIT_SET(tmr_con[timer_id], TMR_ON_BIT);
+    BIT_SET(timer_intreg[timer_id].enable, timer_intreg[timer_id].ibit);
+  }
 
   return 0;
 }
 
-int timer_stop(pic32_timer_t timer_id)
+int timer_stop(int timer_id)
 {
-   if (timer_id < PIC32_TIMER_2 || timer_id > PIC32_TIMER_9)
-  {
-    return -1;
-  }
+  is_idvalid(timer_id);
 
   BIT_CLR(tmr_con[timer_id], TMR_ON_BIT);
 
@@ -277,6 +346,9 @@ void timer_2_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_2] != NULL)
     timer_int_cb[PIC32_TIMER_2]();
+
+  if (timer_int_oneshot[PIC32_TIMER_2] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_2], TMR_ON_BIT);
 }
 #endif
 
@@ -285,6 +357,9 @@ void timer_3_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_3] != NULL)
     timer_int_cb[PIC32_TIMER_3]();
+
+  if (timer_int_oneshot[PIC32_TIMER_3] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_3], TMR_ON_BIT);
 }
 #endif
 
@@ -293,6 +368,9 @@ void timer_4_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_4] != NULL)
     timer_int_cb[PIC32_TIMER_4]();
+
+  if (timer_int_oneshot[PIC32_TIMER_4] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_4], TMR_ON_BIT);
 }
 #endif
 
@@ -301,6 +379,9 @@ void timer_5_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_5] != NULL)
     timer_int_cb[PIC32_TIMER_5]();
+
+  if (timer_int_oneshot[PIC32_TIMER_5] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_5], TMR_ON_BIT);
 }
 #endif
 
@@ -309,6 +390,9 @@ void timer_6_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_6] != NULL)
     timer_int_cb[PIC32_TIMER_6]();
+
+  if (timer_int_oneshot[PIC32_TIMER_6] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_6], TMR_ON_BIT);
 }
 #endif
 
@@ -317,6 +401,9 @@ void timer_7_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_7] != NULL)
     timer_int_cb[PIC32_TIMER_7]();
+
+  if (timer_int_oneshot[PIC32_TIMER_7] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_7], TMR_ON_BIT);
 }
 #endif
 
@@ -325,6 +412,9 @@ void timer_8_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_8] != NULL)
     timer_int_cb[PIC32_TIMER_8]();
+
+  if (timer_int_oneshot[PIC32_TIMER_8] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_8], TMR_ON_BIT);
 }
 #endif
 
@@ -333,6 +423,9 @@ void timer_9_callback(void)
 {
   if (timer_int_cb[PIC32_TIMER_9] != NULL)
     timer_int_cb[PIC32_TIMER_9]();
+
+  if (timer_int_oneshot[PIC32_TIMER_9] == true)
+    BIT_CLR(tmr_con[PIC32_TIMER_9], TMR_ON_BIT);
 }
 #endif
 
